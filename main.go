b63aebfec7bf0ff516df4e083df0ba666cb466f6
michaelgbenle <michaelgbenle@gmail.com>
2022-10-06 10:28:46 -0700

    ...

--- main.go
+++ main.go
@@ -23,24 +23,22 @@ func main() {
 	  log.Fatal("Error loading .env file")
 	}
 
-	googleId := os.Getenv("google-client-id")
-  googleSecret := os.Getenv("google-client-secret")
-  
-  key :=os.Getenv("Secret-session-key")  // Replace with your SESSION_SECRET or similar
-  maxAge := 86400 * 30  // 30 days
-  isProd := false       // Set to true when serving over https
+	 googleId := os.Getenv("google-client-id")
+  	 googleSecret := os.Getenv("google-client-secret")
+	 key :=os.Getenv("Secret-session-key")  // Replace with your SESSION_SECRET or similar
+  	 maxAge := 86400 * 30  // 30 days
+  	 isProd := false       // Set to true when serving over https
+	 store := sessions.NewCookieStore([]byte(key))
+  	store.MaxAge(maxAge)
+  	store.Options.Path = "/"
+  	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+  	store.Options.Secure = isProd
 
-  store := sessions.NewCookieStore([]byte(key))
-  store.MaxAge(maxAge)
-  store.Options.Path = "/"
-  store.Options.HttpOnly = true   // HttpOnly should always be enabled
-  store.Options.Secure = isProd
+  	gothic.Store = store
 
-  gothic.Store = store
-
-  goth.UseProviders(
+  	goth.UseProviders(
     google.New(googleId, googleSecret, "http://localhost:3000/auth/google/callback", "email", "profile"),
-  )
+  	)
 
   p := pat.New()
   p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
