e85a3bcdf8e5a9a79e4758ec2b37a9cf0ded6bac
michaelgbenle <michaelgbenle@gmail.com>
2022-10-06 11:30:29 -0700

    ...

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+
+func HandlerSuccess(res http.ResponseWriter, req *http.Request) {
+
+	user, err := gothic.CompleteUserAuth(res, req)
+	if err != nil {
+		fmt.Fprintln(res, err)
+		return
+	}
+	t, _ := template.ParseFiles("templates/success.html")
+	t.Execute(res, user)
+}
